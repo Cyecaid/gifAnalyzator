3a087fbbac96e4c37cfa52747e759f1975d1c818
Cyeciad <loginmish@gmail.com>
2025-01-09 22:44:40 +0500

    changes in lzw_decoder_test

--- Test/test_lzw_decoder.py
+++ Test/test_lzw_decoder.py
@@ -1,26 +1,24 @@
-import unittest
-
 from lzw_decoder import LZWDecoder
 
 
-class TestLZWDecompressor(unittest.TestCase):
-    def test_empty_data(self):
-        expected = []
-        decompressor = LZWDecoder(2, [])
-        result = decompressor.decode()
-        self.assertEqual(expected, result)
+def test_empty_data():
+    expected = []
+    decompressor = LZWDecoder(2, [])
+    result = decompressor.decode()
+    assert result == expected
 
-    def test_only_clear_and_end_codes(self):
-        expected = [0]
-        data = b'D\x01'
-        decompressor = LZWDecoder(2, data)
-        result = decompressor.decode()
-        self.assertEqual(expected, result)
 
-    def test_big_data(self):
-        expected = [0]
-        data = b'D\x01' * 1000
-        decompressor = LZWDecoder(2, data)
-        result = decompressor.decode()
-        self.assertEqual(expected, result)
+def test_only_clear_and_end_codes():
+    expected = [0]
+    data = b'D\x01'
+    decompressor = LZWDecoder(2, data)
+    result = decompressor.decode()
+    assert result == expected
 
+
+def test_big_data():
+    expected = [0]
+    data = b'D\x01' * 1000
+    decompressor = LZWDecoder(2, data)
+    result = decompressor.decode()
+    assert result == expected
